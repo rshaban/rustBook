7585f8a89c109ed2f3684313e4ce3d05f997dad7
ramsey shaban <44956449+rshaban@users.noreply.github.com>
2021-01-09 14:26:42 -0500

    begin user input exercise

--- uwu/src/main.rs
+++ uwu/src/main.rs
@@ -1,8 +1,24 @@
-use rand::Rng;
+// use rand::Rng;
+use std::io;
 
 fn main() {
+    /* 1/8/21 
     let mut rand = rand::thread_rng().gen_range(1,101);
     println!("rand = {}", rand);
     rand = rand::thread_rng().gen_range(1,101);
     println!("rand = {}", rand);
+    */
+
+    /* 1/9/21 */
+    let mut input = String::new();
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    io::stdin()
+        .read_line(&mut input)
+        .expect("error reading from stdin");
+
+    // take input number and output corresponding month
+    let parseInput: u32 = input.parse().unwrap(); //need error handling
+    println!("{}",parseInput);
+    
+
 }
