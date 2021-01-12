afde22133600e485c333ba7a923957b28e5f2f22
ramsey shaban <44956449+rshaban@users.noreply.github.com>
2021-01-12 10:36:19 -0500

    add user prompts

--- uwu/src/main.rs
+++ uwu/src/main.rs
@@ -12,15 +12,15 @@ fn main() {
     /* 1/9/21 */
     let mut input = String::new();
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    println!("Enter a number 1-12: ");
     io::stdin()
         .read_line(&mut input)
         .expect("error reading from stdin");
 
     // take input number and output corresponding month
     let parse_input: usize = input.trim().parse().unwrap(); //need error handling
-    
     if parse_input > 0 && parse_input < 13 {
-        println!("{}", months[parse_input]);
+        println!("That corresponds to the month of {}", months[parse_input - 1]); // -1 since index starts at 0
     } else {
         println!("not in month range");
     }
